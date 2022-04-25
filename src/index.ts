f36f7caa08eb4d86ee7fa32be2a5ea00a8accfa0
not-matthias <26800596+not-matthias@users.noreply.github.com>
2022-04-25 11:31:43 +0200

    feat: add basic server with socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import * as dotenv from "dotenv";
 import express from "express";
+import { Server } from "socket.io";
+import * as http from "http";
 
-dotenv.config({ path: __dirname + '/../.env' });
+dotenv.config({ path: __dirname + "/../.env" });
 
 const app = express();
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
-app.use('/dsvc/healthcheck', require('express-healthcheck')());
+app.use("/ntfy/healthcheck", require("express-healthcheck")());
+
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.of("/ntfy/ws").on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(process.env.PORT || 8080, () => {
+  console.log(`Server is running on port ${process.env.PORT || 8080}`);
+});
